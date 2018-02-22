fe414bcfaf617b0aca2094d0525a80979dfa23e2
kenanbirkan <kenanbirkan@example.com>
2018-02-22 14:21:41 +0300

    update

--- test_keras/vgg16/vgg_16_main.py
+++ test_keras/vgg16/vgg_16_main.py
@@ -11,7 +11,7 @@ from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 import ntpath
 from vgg16_bottleneck import top_model_weights_path, train_data_dir, validation_data_dir, img_height, img_width, bft_file, bfv_file
-from keras.callbacks import EarlyStopping,ModelCheckpoint
+from keras.callbacks import  EarlyStopping,ModelCheckpoint
 # number of epochs to train top model
 epochs = 100 # TODO update
 # batch size used by flow_from_directory and predict_generator
@@ -88,7 +88,7 @@ def train_top_model():
         samples_per_epoch=nb_train_samples,
         epochs=epochs,
         validation_data=validation_generator,
-        nb_val_samples=nb_validation_samples,
+        validation_steps=nb_validation_samples,
         callbacks=[checkpoint, early])
 
     end_time = datetime.datetime.utcnow()
@@ -99,62 +99,12 @@ def train_top_model():
     print("Loss: ", score[0], "Accuracy: ", score[1])
 
 
-def predict(image_path, model=None):
-    if not model:
-        model = load_model("keras4_100epoch.h5")
-
-    class_dictionary = np.load('class_indices.npy').item()
-    print("[INFO] loading and preprocessing image...")
-    image = load_img(image_path, target_size=(img_width, img_height))
-    image = img_to_array(image)
-
-    # important! otherwise the predictions will be '0'
-    image = image / 255
-
-    image = np.expand_dims(image, axis=0)
-    prediction = model.predict(image)
-
-    class_predicted = np.argmax(prediction, axis=1)
-    inID = class_predicted[0]
-
-    inv_map = {v: k for k, v in class_dictionary.items()}
-
-    label = inv_map[inID]
-
-    # get the prediction label
-    print("Image ID: {}, Label: {}".format(inID, label))
-    return label
-
-
-def predict_folder_path(walk_dir):
-    result_list = []
-    model = load_model("keras4.h5")
-
-    for root, subdirs, files in os.walk(walk_dir):
-        positive = 0
-        negative = 0
-        result_dict = {}
-        true_label = None
-        for file_name in files:
-            head, true_label = ntpath.split(root)
-            full_path = os.path.join(root, file_name)
-            label = predict(full_path, model=model)
-            if label == true_label:
-                positive += 1
-            else:
-                negative += 1
-
-        if true_label:
-            result_dict[true_label + ":positive"] = positive
-            result_dict[true_label + ":negative"] = negative
-            result_list.append(result_dict)
-
-    print result_list
-
 
 if __name__ == '__main__':
     from vgg16_bottleneck import save_bottleneck_features, train_top_model_bottleneck
 
-    save_bottleneck_features()
-    train_top_model_bottleneck()
-    train_top_model()
+    #save_bottleneck_features()
+    #train_top_model_bottleneck()
+    #train_top_model()
+    from test_keras.tools.utils import evaluate_model,predict_folder_path,predict
+    predict_folder_path(walk_dir="/home/cihan/Desktop/DATAFOLDER/test",model_path="/home/cihan/Desktop/vgg16.h5")
